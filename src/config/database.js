37aff2c1af1ecc5480ecaa13fde7869db0a292e8
Benja <birivera@uc.cl>
2021-04-21 09:34:28 -0400

    Changed prod config to use env_var DB URI

--- src/config/database.js
+++ src/config/database.js
@@ -12,10 +12,11 @@ module.exports = {
     dialect: process.env.DB_DIALECT || 'postgres',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: 'DATABASE_URL',  // Usa URI DB en vez de config manual
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT || 'postgres',
   },
 }
