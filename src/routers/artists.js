ffd8c631298b16c18249c6961d98630f724b3930
Benja <birivera@uc.cl>
2021-04-22 12:17:31 -0400

    Added artist/:id endpoint

--- src/routers/artists.js
+++ src/routers/artists.js
@@ -7,13 +7,36 @@ const router = express.Router()
 // Todas las rutas de esta categoría ----------------------
 
 // GET /artists. Get all artists
-router.get(`/`, async function (req, res) {
-  // res.send('Hello artists!');
+router.get(
+  `/`,
+  async function (req, res) {
+
+    const artists = await req.models.tartist.findAll({
+      attributes: ['id', 'name', 'age', 'albums', 'tracks', 'self'],
+    });
+    // res.body = artists;
+    res.send(artists);
+  }
+)
+
+// GET /artists/:id. Get all artist by id
+router.get(`/:id`, async function (req, res) {
+
+  // console.log('req', req);
+  const id = req.params.id;
+  console.log('preguntando por el id:', id);
   const artists = await req.models.tartist.findAll({
     attributes: ['id', 'name', 'age', 'albums', 'tracks', 'self'],
+    where: {
+      id
+    }
   });
-  // res.body = artists;
-  res.send(artists);
+  if (artists.length > 0) {
+    res.send(artists);
+  } else {
+    res.status(404);
+    res.send('Artista no encontrado')
+  }
 })
 
 router.post(
