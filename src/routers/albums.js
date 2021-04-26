74f05c20f77b05e3984bc7ce950227709fc6d9b7
Benja <birivera@uc.cl>
2021-04-26 11:44:29 -0400

    Added GET albums route

--- src/routers/albums.js
+++ src/routers/albums.js
@@ -2,10 +2,20 @@ var express = require('express');
 
 const router = express.Router()
 
-// Todas las rutas de esta categoría
-router.get(`/`, function (req, res) {
-  res.send('Hello albums!');
-})
+// Todas las rutas de esta categoría ---------------------------
+
+// GET /albums
+router.get(
+  `/`,
+  async function (req, res) {
+    const albums = await req.models.talbum.findAll({
+      attributes: ['id', ['tartist_id', 'artist_id'], 'name', 'genre', 'artist', 'tracks', 'self'],
+    })
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(albums);
+  }
+)
 
 
 
