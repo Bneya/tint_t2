b74ca4aa239b0199fb33fa1d59858852471a92db
Benja <birivera@uc.cl>
2021-04-26 11:55:37 -0400

    Added GET tracks and tracks/id routes

--- src/routers/tracks.js
+++ src/routers/tracks.js
@@ -2,11 +2,45 @@ var express = require('express');
 
 const router = express.Router()
 
-// Todas las rutas de esta categoría
-router.get(`/`, function (req, res) {
-  res.send('Hello tracks!');
-})
+// Todas las rutas de esta categoría -----------------------
 
+
+// GET /tracks
+router.get(
+  `/`,
+  async function (req, res) {
+    const tracks = await req.models.ttrack.findAll({
+      attributes: ['id', ['talbum_id', 'album_id'], 'name', 'duration', 'times_played', 'artist', 'album', 'self'],
+    })
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(tracks);
+  }
+)
+
+// GET /tracks/:id. Get track by id
+router.get(
+  `/:id`,
+  async function (req, res) {
+
+    const id = req.params.id;
+    console.log('preguntando por el id:', id);
+    const track = await req.models.ttrack.findOne({
+      attributes: ['id', ['talbum_id', 'album_id'], 'name', 'duration', 'times_played', 'artist', 'album', 'self'],
+      where: {
+        id
+      }
+    });
+
+    if (track) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(track);
+    } else {
+      res.status(404);
+      res.send('Canción no encontrada')
+    }
+  }
+)
 
 
 
